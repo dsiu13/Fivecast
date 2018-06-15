f4ebe7a7cd33d044d7d6e78641ea230ca761a7ba
IamZombiez <dereksiu216@gmail.com>
2018-06-14 18:49:41 -0700

    files

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+const API_KEY = '';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
+
+// Redux-Promise Middleware
+// If the payload has a promise, redux-promise stops the action.
+// After the promise resolves, it creates a new action and sends it to reducers
+// Otherwise it goes straight to the reducers
